56527ed77f20d5f96b9287c5135d821277608f81
Mizu49 <mizuki.abe.r4@dc.tohoku.ac.jp>
2021-06-27 11:34:26 +0000

    modify repo setting of `deploydocs()` in `make.jl`

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,7 @@ makedocs(
 )
 
 deploydocs(
-    repo = "github.com/jjj999/PersonalPlayground.jl.git",
+    repo = "github.com/Mizu49/PersonalPlayground.jl.git",
     devbranch = "develop",
     push_preview = true,
 )
